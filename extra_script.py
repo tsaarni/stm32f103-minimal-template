65355cccc0f0a03ba81e7428a54b9e7214f5365b
Tero Saarni <tero.saarni@gmail.com>
2019-04-07 17:14:28 +0300

    updated for new version

--- extra_script.py
+++ extra_script.py
@@ -1,5 +1,6 @@
 Import("env")
 env.Append(LINKFLAGS=["--specs=nano.specs"])
+env.Append(CXXFLAGS=["-Wno-register"])
 
 # for printing floats
 #env.Append(LINKFLAGS=["--specs=nano.specs", "-u", "_printf_float"])
